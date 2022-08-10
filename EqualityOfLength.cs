fee1476c4d69afc5978977adea69dd07e89584ff
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 18:31:58 +0530

    Day-8 UC-2  Added

---
+++ EqualityOfLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionUsingNonStatic
+{
+    public class EqualityOfLength
+    {
+        public void CheckEqualityOfLength(int a1, int b1, int a2, int b2)
+        {
+            int len1, len2;
+            if (a1 == a2)
+            {
+                Console.WriteLine(b1.Equals(b2));
+            }
+            else
+            {
+                len1 = b1 - a1;
+                len2 = b2 - a2;
+                Console.WriteLine(len1.Equals(len2));
+              
+            }
+        }
+    }
+}
