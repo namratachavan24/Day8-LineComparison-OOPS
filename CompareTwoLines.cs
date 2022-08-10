629856c166a9ecbd38f0553a49b4e7b903b5d2f5
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 18:33:40 +0530

    Day-8 UC-3  Added

---
+++ CompareTwoLines.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionUsingNonStatic
+{
+    public class CompareTwoLines
+    {
+        public void CheckTwoLines(int l1, int m1, int l2, int m2)
+        {
+            int le1, le2;
+            if (l1 == l2)
+            {
+                int comp = m1.CompareTo(m2);
+                if (comp == 0)
+                {
+                    Console.WriteLine("Both line are equals");
+                }
+                else if (comp >= 0)
+                {
+                    Console.WriteLine("Second line is Smaller than first");
+                }
+                else
+                {
+                    Console.WriteLine("Second line is bigger than First");
+                }
+            }
+            else
+            {
+                le1 = m1 - l1;
+                le2 = m2 - l2;
+                int comp = le1.CompareTo(le2);
+                if (comp == 0)
+                {
+                    Console.WriteLine("Both line are equals");
+                }
+                else if (comp >= 0)
+                {
+                    Console.WriteLine("Second line is Smaller than first");
+                }
+                else
+                {
+                    Console.WriteLine("Second line is bigger than First");
+                }
+            }
+        }
+    }
+}
